56d0943cbffbf5b9415929ffa466a490a69d85b6
ruanzindeluxe <128759460+ruanzinn7b@users.noreply.github.com>
2023-06-17 01:17:22 -0300

    Create senhaatualizada.php

---
+++ senhaatualizada.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Senha Atualizada</title>
+    <style>
+        body {
+            background-image: url('https://rare-gallery.com/livewalls/imgpreview/145212-hashira-giyu-tomioka-with-katana-water-4k.jpg');
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        center {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 5px;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+            color: white; /* Altera a cor do texto para branco */
+        }
+
+        a {
+            text-decoration: none;
+            color: #FF0000; /* Altera a cor do texto para vermelho */
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1 style="color: white;">Senha Atualizada com Sucesso!</h1>
+        <a href="login.php">Voltar para a página de login</a>
+    </center>
+</body>
+</html>
